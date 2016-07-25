68aeb373bda05288c056dbd360fa3dc6c2455f0e
mikewiltero <mike@unftalon.org>
2016-07-25 00:32:20 +0000

    figured out how to interface the TSS API with the ROS sensor_msgs/Imu data type to be published.

--- drivers/talon_tss/scripts/test2.py
+++ drivers/talon_tss/scripts/test2.py
@@ -1,32 +1,77 @@
+#!/usr/bin/env python
 # tss_test2.py this is a ROS node that will make sure everything is working properly.
 # TODO: make it more intellegient so it will automatically known 
 # what port the tss is connected to.
-#!/usr/bin/env python2
+
 
 
 import rospy
-# import sensor_msgs IMU
+from sensor_msgs.msg import Imu
 import sys
 import time
+from std_msgs.msg import String
+import threespace as ts_api
 
-def callback(data):
-		
-		
+device = ts_api.TSUSBSensor(com_port='/dev/ttyACM0')
+
 def tss_node():
 	# Initialize a ROS node
+	pub = rospy.Publisher('ImuData', Imu, queue_size=10)
 	rospy.init_node('tss_node')
+
 	
-	# set up a subscriber of the topic "imu", type "Imu" and assign the callback function
-	rospy.Subscriber("imu", Imu, callback)
 	
-	global pub
-	pub = rospy.Publisher('imuData', Imu, queue_size=10)
 	
 	r = rospy.Rate(10)
 	while not rospy.is_shutdown():
+		#ledTup = device.getLEDColor()
+		#led = list(ledTup)
+		imuMsg = Imu()
+		orientation = list(device.getTaredOrientationAsQuaternion())
+		
+
+		
+		imuMsg.orientation.x = orientation[0]
+		imuMsg.orientation.y = orientation[1]
+		imuMsg.orientation.z = orientation[2]
+		imuMsg.orientation.w = orientation[3]
+		
+		pub.publish(imuMsg)
+		
+		
+		#print(led)
+		#pub.publish(led)
 		r.sleep()
 		
 	rospy.spin()
 	
 if __name__ == '__main__':
 	tss_node()
+
+
+#if device is not None:
+    ## Now we can start getting information from the device.
+    ## The class instances have all of the functionality that corresponds to the
+    ## 3-Space Sensor device type it is representing.
+    #print("It looks like we've connected.")
+    #print("==================================================")
+    #print("Getting the filtered tared quaternion orientation.")
+    #quat = device.getTaredOrientationAsQuaternion()
+    #if quat is not None:
+    #    print(quat)
+    #print("==================================================")
+    #print("Getting the raw sensor data.")
+    #data = device.getAllRawComponentSensorData()
+    #if data is not None:
+    #    print("[%f, %f, %f] --Gyro\n"
+    #          "[%f, %f, %f] --Accel\n"
+    #          "[%f, %f, %f] --Comp" % data)
+    #print("==================================================")
+    #print("Getting the LED color of the device.")
+    #led = device.getLEDColor()
+    #if led is not None:
+    #    print(led)
+    #print("==================================================")
+    
+    ## Now close the port.
+    
