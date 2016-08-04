9180e8a40285424635aba9489434ca2b84396bf9
Michael Otero <mike@kr-tech.net>
2016-08-03 21:56:20 -0400

    ros publisher workingq

--- embedded/talon_bar30/talon_bar30.ino
+++ embedded/talon_bar30/talon_bar30.ino
@@ -3,21 +3,34 @@
     UNF TALON
 */
 
+#include <ros.h>
+#include <sensor_msgs/FluidPressure.h>
 #include <MS5837.h>
 #include <i2c_t3.h>
+
+// ROS things ================================================
+ros::NodeHandle nh;
+sensor_msgs::FluidPressure bar30Value; // this  name will change later
+ros::Publisher bar30sens("bar30sens", &bar30Value);
 
 MS5837 bar30;
 
 void setup() {
+
+  nh.initNode();
+  nh.advertise(bar30sens);
+
+  // open serials
   Serial.begin(9600);
-
   Serial.println("Starting");
-
   Wire.begin();
 
+  // initialize the bar30 sensor
   bar30.init();
 
-  bar30.setFluidDensity(1); // kg/m^3 (997 freshwater, 1029 for seawater)
+  // set the fluid density
+  // not sure if floats are accepted here, need to check documentation
+  bar30.setFluidDensity(1); // kg/m^3 (997 freshwater, 1029 for seawater, 1 for air(??))
 
 }
 
@@ -42,4 +55,9 @@ void loop() {
 
   delay(1000);
 
+  
+  bar30Value.fluid_pressure = bar30.depth(); // assigning the value we get to the thing that will be published
+  bar30Value.variance = 0;
+  bar30sens.publish( &bar30Value );
+  nh.spinOnce();
 }
