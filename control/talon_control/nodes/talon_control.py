841e1adb38280bf2369184d7da1a327d6525192e
Michael Otero <mike@kr-tech.net>
2016-08-15 12:58:05 -0400

    added code

--- control/talon_control/nodes/talon_control.py
+++ control/talon_control/nodes/talon_control.py
@@ -14,3 +14,17 @@ def trajectory_cb(msg):
 	this is the callback for the trajectory subscription
 	'''
 	
+def talon_controller():
+	'''
+	this is the main for talon_controller node
+	'''
+	
+	rospy.init_node('talon_controller', anonymous=False)
+	
+	rospy.Subscriber("odom", String, odom_cb)
+	rospy.Subscriber("trajectory", String, trajectory_cb)
+	
+	rospy.spin()
+	
+if __name__ == '__main__':
+	talon_controller():
