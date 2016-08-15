cdaf229e229e81e103066139841842ba3702a0c8
contriv4nce <mike@kr-tech.net>
2016-08-15 15:43:59 -0400

    changed talon_control.py to a class and trying to get the arguments to pass right. main function is not working properly

---
+++ control/talon_control/nodes/pid_tester.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# for testing the talon_pid node
+
+
+
+import rospy
+from geometry_msgs.msg import Pose
+import sys
+import time
+	
+ 
+def pid_tester():
+	# Initialize a ROS node
+	rospy.init_node('pid_tester')
+
+	pub = rospy.Publisher('trajectory', Pose, queue_size=10)
+	
+	
+ 
+	# reduce the rate of the loop and keep it alive
+	r = rospy.Rate(10) # 10hz
+	while not rospy.is_shutdown():
+		r.sleep()
+		
+		d = Pose()
+		d.position.x = 1
+		d.position.y = 2
+		d.position.z = 3
+		d.orientation.x = 4
+		d.orientation.y = 5
+		d.orientation.z = 6
+		d.orientation.w = 7
+		pub.publish(d)
+		
+		
+	# spin() simply keeps python from exiting until this node is stopped
+	rospy.spin()
+ 
+if __name__ == '__main__':
+	pid_tester()
+
--- control/talon_control/nodes/talon_control.py
+++ control/talon_control/nodes/talon_control.py
@@ -3,51 +3,82 @@
 import rospy
 import sys
 import time
+import numpy
 from std_msgs.msg import String
-
+from geometry_msgs.msg import Pose
 from talon_pid import PID
 
-def odom_cb(msg):
-	'''
-	this is for getting the odometry data on subscribe
-	'''
-	
-	rospy.loginfo(rospy.get_caller_id() + "I head %s", msg.data)
-	
-def trajectory_cb(msg):
-	'''
-	this is the callback for the trajectory subscription
-	'''
-	
-	# get desired trajectory from msg
-	# assign to variable
-	
-def talon_controller():
-	'''
-	this is the main for talon_controller node
-	'''
-	
-	rospy.init_node('talon_controller', anonymous=False)
-	
-	rospy.Subscriber("odom", String, odom_cb, queue_size=1)
-	rospy.Subscriber("trajectory", String, trajectory_cb, queue_size=1)
-	rospy.Publisher("wrench", String, queue_size=1)
-	
-	
-	
-	# this is our main thing
-	
-	# calculate errors
-	
-	# combine errors into the same array
-	
-	# send errors through PID controller
-	
-	# combine into final wrench (for output)
-	
-	# publish values
-	
-	rospy.spin()
-	
+
+
+
+
+# this needs to be moved to some common library somewhere
+# it quickly breaks a message into an array
+xyz_array = lambda o: numpy.array([o.x, o.y, o.z])
+xyzw_array = lambda o: numpy.array([o.x, o.y, o.z, o.w])
+
+rospy.init_node('talon_controller', anonymous=False)
+
+class talon_control:
+
+	def __init__(self):
+		# Create arrays to be used
+		self.desired_state = numpy.ones(6)
+		self.desired_velocity = numpy.ones(6)
+		self.current_state = numpy.zeros(6)
+		self.current_velocity = numpy.zeros(6)
+		self.current_error = numpy.ones(0)
+		
+		# ROS components
+		rospy.Subscriber("odom", String, self.odom_cb, queue_size=1)
+		rospy.Subscriber("trajectory", Pose, self.trajectory_cb, queue_size=1)
+		self.controller_wrench = rospy.Publisher("wrench", Pose, queue_size=1)
+
+
+	def odom_cb(self, msg):
+		'''
+		this is for getting the odometry data on subscribe
+		'''
+		
+		rospy.loginfo(rospy.get_caller_id() + "I head %s", msg.data)
+		
+	def trajectory_cb(self, desired_trajectory):
+		'''
+		this is the callback for the trajectory subscription
+		'''
+		# Get desired pose and orientation
+		
+		self.desired_pose = xyz_array(desired_trajectory.position) # desired pose
+		self.desired_orientation = xyzw_array(desired_trajectory.orientation) #desired orientation
+
+		
+		
+	def talon_controller(self, event):
+		'''
+		this is the main for talon_controller node
+		'''
+		
+
+		
+
+		
+		# this is our main thing
+		
+		# calculate errors
+		
+		# combine errors into the same array
+		
+		# send errors through PID controller
+		
+		# combine into final wrench (for output)
+		
+		# publish values
+		#print self.desired_pose
+		self.controller_wrench.publish(desired_pose)
+		
+		
+		
 if __name__ == '__main__':
-	talon_controller()
+	controller = talon_control()
+	rospy.Timer(rospy.Duration(1.0/50.0), controller.talon_controller)
+	rospy.spin()
