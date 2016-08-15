645aa4df10c031dc2bc855b1eab2ceb1de2b6580
contriv4nce <mike@kr-tech.net>
2016-08-15 14:23:01 -0400

    added comments that helped me understand better

--- control/talon_control/nodes/talon_control.py
+++ control/talon_control/nodes/talon_control.py
@@ -5,6 +5,8 @@ import sys
 import time
 from std_msgs.msg import String
 
+from talon_pid import PID
+
 def odom_cb(msg):
 	'''
 	this is for getting the odometry data on subscribe
@@ -17,6 +19,9 @@ def trajectory_cb(msg):
 	this is the callback for the trajectory subscription
 	'''
 	
+	# get desired trajectory from msg
+	# assign to variable
+	
 def talon_controller():
 	'''
 	this is the main for talon_controller node
@@ -27,6 +32,20 @@ def talon_controller():
 	rospy.Subscriber("odom", String, odom_cb, queue_size=1)
 	rospy.Subscriber("trajectory", String, trajectory_cb, queue_size=1)
 	rospy.Publisher("wrench", String, queue_size=1)
+	
+	
+	
+	# this is our main thing
+	
+	# calculate errors
+	
+	# combine errors into the same array
+	
+	# send errors through PID controller
+	
+	# combine into final wrench (for output)
+	
+	# publish values
 	
 	rospy.spin()
 	
