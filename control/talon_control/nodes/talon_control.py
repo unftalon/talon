255f756fda5ed71f7f36d53837e3b55e3e94b2ee
Michael Otero <mike@kr-tech.net>
2016-08-15 12:50:20 -0400

    initial commit

---
+++ control/talon_control/nodes/talon_control.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import rospy
+import sys
+import time
+
+def odom_cb(msg):
+	'''
+	this is for getting the odometry data on subscribe
+	'''
+	
+def trajectory_cb(msg):
+	'''
+	this is the callback for the trajectory subscription
+	'''
+	
