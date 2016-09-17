7b1a98700413487f876feb4954de0cdf5cb0c059
mikewiltero <mike@kr-tech.net>
2016-09-17 18:52:49 -0400

    added comments

--- vision/talon_vision/scripts/fv_filter_server.py
+++ vision/talon_vision/scripts/fv_filter_server.py
@@ -21,26 +21,32 @@ from cv_bridge import CvBridge, CvBridgeError # NEED TO SET THIS UP TOO
 class fv_filter_server:
 	
 	def __init__(self):
+		# set up publisher and subscriber
 		self.image_pub = rospy.Publisher("/vision/fwd/img_filtered", Image, queue_size=1)
-		
-		self.bridge = CvBridge()
 		self.image_sub = rospy.Subscriber("/camera/image_raw", Image, self.callback)
 		
+		# instantiate the CvBridge
+		self.bridge = CvBridge()
+		
+	# callback function for subscriber.
 	def callback(self, data):
+	
+		# try to convert the ROS image to a cv image and throw exception if it fails
 		try:
 			cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
 		except CvBridgeError as e:
 			print(e)
 		
-		# do some things hewre
+		# this is a test modification. draws some circle on the top left of the image
 		(rows,cols,channels) = cv_image.shape
 		if cols > 60 and rows > 60 :
 			cv2.circle(cv_image, (50,50), 10, 255)
 			
-		
+		# create a window in the GUI to show the cv image
 		cv2.imshow("Image Window", cv_image)
 		cv2.waitKey(3)
 		
+		# try to convert the image back to ROS format and publish it.
 		try:
 			self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
 		except CvBridgeError as e:
