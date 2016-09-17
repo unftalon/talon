88248e2679c15f3c6bdd2416d636d83bf77b1a7f
mikewiltero <mike@kr-tech.net>
2016-09-17 19:17:37 -0400

    initial commit

---
+++ vision/talon_vision/scripts/fv_bouy_detect.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#
+# fv_bouy_detect.py
+# Forward Vision buoy detector
+# 
+# This node will subscribe to filtered camera images, detect the bouy, and send information about its location
+#
+# TODO: 
+#
+# written by Michael Otero
+# email: mike at unftalon.org
+# credit to some other authors that I will list here later. (uf-mil)
