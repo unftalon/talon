ee8292bd75c535b227834f36b738086fa87254b5
mikewiltero <mike@kr-tech.net>
2016-09-17 22:28:02 -0400

    added openCV gui to allow on the fly adjustment of color. this allowed obtaining of HSV values of red ball

--- vision/talon_vision/scripts/fv_qgate_detect.py
+++ vision/talon_vision/scripts/fv_qgate_detect.py
@@ -16,6 +16,10 @@ import rospy
 import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+
+def nothing(x):
+	pass
 
 class fv_qgate_detect:
 
@@ -26,6 +30,14 @@ class fv_qgate_detect:
 		
 		self.bridge = CvBridge()
 		
+		cv2.namedWindow('Trackbars')
+		cv2.createTrackbar('lower_hue','Trackbars',0,179, nothing)
+		cv2.createTrackbar('lower_sat','Trackbars',0,255, nothing)
+		cv2.createTrackbar('lower_val','Trackbars',0,255, nothing)
+		cv2.createTrackbar('upper_hue','Trackbars',0,179, nothing)
+		cv2.createTrackbar('upper_sat','Trackbars',0,255, nothing)
+		cv2.createTrackbar('upper_val','Trackbars',0,255, nothing)
+		
 	def callback(self, data):
 	
 		# try to convert the ROS image to a cv image and throw exception if it fails
@@ -33,11 +45,35 @@ class fv_qgate_detect:
 			cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
 		except CvBridgeError as e:
 			print(e)
-			
-		blur = cv2.GaussianBlur(cv_image, (5,5),0)
 		
+		hl = cv2.getTrackbarPos('lower_hue','Trackbars')
+		sl = cv2.getTrackbarPos('lower_sat','Trackbars')
+		vl = cv2.getTrackbarPos('lower_val','Trackbars')
+		hu = cv2.getTrackbarPos('upper_hue','Trackbars')
+		su = cv2.getTrackbarPos('upper_sat','Trackbars')
+		vu = cv2.getTrackbarPos('upper_val','Trackbars')
+		
+		blur = cv2.GaussianBlur(cv_image, (5,5),0)
+		hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+		
+		''' so far it looks like the red ball has the following upper and lower limits for HSV:
+		
+			lower: 4,150,130
+			upper: 15,230,243
+			
+		'''
+		lower_green = np.array([hl,sl,vl])
+		upper_green = np.array([hu,su,vu])
+		
+		mask = cv2.inRange(hsv, lower_green, upper_green)
+		
+		bmask = cv2.GaussianBlur(mask, (5,5),0)
+		
+		
+		
+
 		# create a window in the GUI to show the cv image
-		cv2.imshow("Image Window", blur)
+		cv2.imshow("Image Window", bmask)
 		cv2.waitKey(3)		
 			
 			
@@ -45,11 +81,13 @@ class fv_qgate_detect:
 def main(args):
 	fqd = fv_qgate_detect()
 	rospy.init_node('fv_qgate_detect', anonymous=True)
+	
 	try:
 		rospy.spin()
 	except KeyboardInterrupt:
 		print("shutting down")
 	cv2.destroyAllWindows()
+
 	
 if __name__=='__main__':
 	main(sys.argv)
