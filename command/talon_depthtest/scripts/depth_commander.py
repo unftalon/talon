950190ee834df41002e38eed60e4398dd43d6cbd
contriv4nce <mike@kr-tech.net>
2016-10-14 19:37:26 -0400

    initial commit

---
+++ command/talon_depthtest/scripts/depth_commander.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#
+# depth_commander.py
+# Depth commander node
+# 
+# This node will subscribe to a commanded depth from various places, calculate
+# the error between it and the actual depth, and send the appropriate commands to a thruster
+# commander (maybe)
+#
+# TODO: 	
+#
+# written by Michael Otero
+# email: mike at unftalon.org
+# 
