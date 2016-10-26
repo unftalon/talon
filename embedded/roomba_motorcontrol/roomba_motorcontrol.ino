f6e9aaf7bc2a395c12aedc8072f31b428ed9eaa0
Michael Otero <mike@kr-tech.net>
2016-10-26 15:58:10 -0400

    motor control kinda works

--- embedded/roomba_motorcontrol/roomba_motorcontrol.ino
+++ embedded/roomba_motorcontrol/roomba_motorcontrol.ino
@@ -14,30 +14,67 @@
 
 #include <ros.h>     // include the ROS library so we can talk to the ROS network
 #include <AFMotor.h> // include the adafruit motor shield library
+#include <geometry_msgs/Twist.h>
 // include any necessary message types here
 
 // ROS things ================================================
 ros::NodeHandle nh;
 
 // declare publisher/subscriber topic names here
-sensor_msgs::FluidPressure bar30pressure;
-sensor_msgs::Temperature bar30temp;
+//sensor_msgs::FluidPressure bar30pressure;
+//sensor_msgs::Temperature bar30temp;
+geometry_msgs::Twist cmd_vel;
 
-ros::Publisher bar30pressure_pub("/sensors/pressure", &bar30pressure);
-ros::Publisher bar30temp_pub("/sensors/water_temp", &bar30temp);
+//ros::Publisher bar30pressure_pub("/sensors/pressure", &bar30pressure);
+//ros::Publisher bar30temp_pub("/sensors/water_temp", &bar30temp);
+
 // ===========================================================
+
+/* setup an array to hold the 6 Twist values
+ *  {linear.x, linear.y, linear.z, angular.x, angular.y, angular.z}
+ */
+int vel[6];
+#define base_width 0.5; // width of wheel base. should be actually measured.
+
+  // set up the motor interface
+AF_DCMotor motorLeft(3); // define motor on channel 4 with 1KHz default PWM
+AF_DCMotor motorRight(4);
+
+void cmd_cb( const geometry_msgs::Twist& cmd_vel) {
+  // things go here for callback
+  vel[0] = cmd_vel.linear.x;
+  vel[5] = cmd_vel.angular.z;
+}
+
+ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", &cmd_cb);
 
 void setup() {
   // initalize the ROS node
   nh.initNode();
+  nh.subscribe(sub);
 
   // advertise the topics
-  nh.advertise(bar30pressure_pub);
-  nh.advertise(bar30temp_pub);
+  // nh.advertise(bar30pressure_pub);
 
-  // set up the motor interface
-  AF_DCMotor motor3(3); // define motor on channel 4 with 1KHz default PWM
-  AF_DCMotor motor4(4);
+
+
+  
+  //Serial.begin(9600);
+
+  // run the motor test function to make sure this works.
+  motorTest();
+}
+
+void motorTest() {
+  //Serial.println("Testing motors...");
+  motorRight.setSpeed(255);
+  motorLeft.setSpeed(255);
+  motorRight.run(FORWARD);
+  motorLeft.run(FORWARD);
+  delay(1000);
+
+  motorRight.run(RELEASE);
+  motorLeft.run(RELEASE);
 }
 
 void loop() {
@@ -46,9 +83,20 @@ void loop() {
    *  
    */
 
+   int right = vel[0] + vel[5]*base_width;
+   int left = vel[0] - vel[5]*base_width;
+   //nh.loginfo(left);
+   motorRight.setSpeed(right);
+   motorRight.run(FORWARD);
+
+   //Serial.print("Right value: ");
+   //Serial.println(right);
+   //Serial.print("Left value: ");
+   //Serial.println(left);
+
    // publish both of the values to their corresponding ROS topics.
-   bar30pressure_pub.publish( &bar30pressure );
-   bar30temp_pub.publish( &bar30temp );
+   //bar30pressure_pub.publish( &bar30pressure );
+   //bar30temp_pub.publish( &bar30temp );
 
   // spin the ros system once. this is important, don't remove it. should be at end of loop.
    nh.spinOnce();
